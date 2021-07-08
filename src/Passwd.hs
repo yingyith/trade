d9436669d54c9c91573a8144240efbd733bfc195
yingyith <yithorigin@aprilthirty60.com>
2021-07-08 10:53:21 +0000

    tijiao

--- src/Passwd.hs
+++
@@ -1,6 +0,0 @@
-module Passwd
-    ( passwd
-    ) where
-import qualified Data.ByteString.Char8 as B
-passwd = "6AEIzoinSfDYRZx96vQT4HZvkMdMnZ0R497k9hRz02UVZsyYDM2sKnern2Jvz55l" 
-sk = "5FkukTJ9qfQTDrgAB63lt22sxkwQrk5XDgF8xcMaCRyZstrhA1skSynPSsNUT8cc"
