6aca57935aa9641de811402e5ada2b2d5f40effd
yingyith <yithorigin@aprilthirty60.com>
2022-02-04 13:31:46 +0000

    w

--- src/general/Passwd.hs
+++
@@ -1,7 +0,0 @@
-module Passwd
-    ( passwd,
-      sk
-    ) where
-import qualified Data.ByteString.Char8 as B
-passwd = "LTc8xLvRz7vQOutIEvPhOwAlyVzp7IsZqAeQS60j7LhH6cGVmNFOL6zLq2KnLYscnx" 
-sk = "KZn3KzZ0jYciJOsnrld1nly9b1ocmrnRocdvBTrPrHZ5JULyV3qfGP6ltn5GPBcRW"
