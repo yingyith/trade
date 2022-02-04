79611fe1e30eb04e0b1e97c6b5e6e45e12fef9c5
yingyith <yithorigin@aprilthirty60.com>
2022-02-04 13:26:48 +0000

    alter take order hapi

--- src/general/Passwd.hs
+++ src/general/Passwd.hs
@@ -3,5 +3,5 @@ module Passwd
       sk
     ) where
 import qualified Data.ByteString.Char8 as B
-passwd = "c8xLvRz7vQOutIEvPhOwAlyVzp7IsZqAeQS60j7LhH6cGVmNFOL6zLq2KnLYscnx" 
-sk = "Zn3KzZ0jYciJOsnrld1nly9b1ocmrnRocdvBTrPrHZ5JULyV3qfGP6ltn5GPBcRW"
+passwd = "Vc8xLvRz7vQOutIEvPhOwAlyVzp7IsZqAeQS60j7LhH6cGVmNFOL6zLq2KnLYscnx" 
+sk = "KZn3KzZ0jYciJOsnrld1nly9b1ocmrnRocdvBTrPrHZ5JULyV3qfGP6ltn5GPBcRW"
