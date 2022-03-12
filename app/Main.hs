058d07943e6a209edbe9f4f81c3d06da86c1325c
yingyith <yithorigin@aprilthirty60.com>
2022-03-12 07:22:30 +0000

    alter now

--- app/Main.hs
+++ app/Main.hs
@@ -91,7 +91,7 @@ main =
     --let aimss = "/stream?streams=adausdt@kline_1m/"  -----------------------------------------------
     --let aimss = "/ws/" ++aas++"?listenkey="  ++ aas -----------------------------------------------
     --let aimss = "/ws/streams=btcusdt@markPrice/"++ "&listenkey="  ++ aas -----------------------------------------------
-    let aimss = "/ws/btcusdt@markPrice/"++ "&listenkey="  ++ aas -----------------------------------------------
+    let aimss = "/ws/adausdt@kline_1m/"++ "&listenkey="  ++ aas -----------------------------------------------
     --"send ping every 30mins"
     -- pass listen key to getSticksToCache and set key ,then do detail on sub handler ,update
     -- loop every 30mins
