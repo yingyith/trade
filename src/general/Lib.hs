f6e94268d731fd56c4539f8db80ce85ac6d3d9ad
yingyith <yithorigin@aprilthirty60.com>
2022-03-13 08:47:39 +0000

    alter now

--- src/general/Lib.hs
+++ src/general/Lib.hs
@@ -19,10 +19,10 @@ getrsi hl = do
   let downdiff =  [(cprice $ (!!i) al) - (cprice $ (!!(i-1)) al  ) | i <- [1,klen-1] ,let idiff = (cprice $ (!!i) al)-(cprice $ (!!(i-1)) al) in idiff < 0] where al = hl
 
   let gain = case (DL.length updiff) of 
-                  x|x<1 -> 0.001 
+                  x|x<1 -> 0.00001 
                   _     -> (abs $ sum updiff)/(fromIntegral $ DL.length updiff :: Double) 
   let loss = case (DL.length downdiff) of 
-                  x|x<1 -> 0.001
+                  x|x<1 -> 0.00001
                   _     -> (abs $ sum downdiff)/(fromIntegral $ DL.length downdiff :: Double)
   liftIO $ print (gain,loss)
   let rs = gain/loss 
