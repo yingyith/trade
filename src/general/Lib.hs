e7b5ba7574a729facf90b21bef27b3dfba3254a9
yingyith <yithorigin@aprilthirty60.com>
2022-03-13 04:34:57 +0000

    alter now

--- src/general/Lib.hs
+++ src/general/Lib.hs
@@ -16,6 +16,7 @@ getrsi hl = do
   let klen = DL.length hl
   let updiff   =  [(cprice $ (!!i) al) - (cprice $ (!!(i-1)) al  ) | i <- [1,klen-1] ,let idiff = (cprice $ (!!i) al)-(cprice $ (!!(i-1)) al) in idiff > 0] where al = hl
   let downdiff =  [(cprice $ (!!i) al) - (cprice $ (!!(i-1)) al  ) | i <- [1,klen-1] ,let idiff = (cprice $ (!!i) al)-(cprice $ (!!(i-1)) al) in idiff < 0] where al = hl
+  print "enter getesi2"
   let gain = sum updiff
   let loss = abs $ sum downdiff
   let rs = gain/loss 
