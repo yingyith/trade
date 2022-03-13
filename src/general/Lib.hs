e98f057c222dd160573e05157f71e934468b3b44
yingyith <yithorigin@aprilthirty60.com>
2022-03-13 08:32:03 +0000

    alter now

--- src/general/Lib.hs
+++ src/general/Lib.hs
@@ -17,8 +17,13 @@ getrsi hl = do
   let klen = 8
   let updiff   =  [(cprice $ (!!i) al) - (cprice $ (!!(i-1)) al  ) | i <- [1,klen-1] ,let idiff = (cprice $ (!!i) al)-(cprice $ (!!(i-1)) al) in idiff > 0] where al = hl
   let downdiff =  [(cprice $ (!!i) al) - (cprice $ (!!(i-1)) al  ) | i <- [1,klen-1] ,let idiff = (cprice $ (!!i) al)-(cprice $ (!!(i-1)) al) in idiff < 0] where al = hl
-  let gain = (sum updiff)/(fromIntegral $ DL.length updiff :: Double) 
-  let loss = (abs $ sum downdiff)/(fromIntegral $ DL.length downdiff :: Double)
+
+  let gain = case (DL.length updiff) of 
+                  x|x<1 -> 0.001 
+                  _     -> (abs $ sum updiff)/(fromIntegral $ DL.length updiff :: Double) 
+  let loss = case (DL.length downdiff) of 
+                  x|x<1 -> 0.001
+                  _     -> (abs $ sum downdiff)/(fromIntegral $ DL.length downdiff :: Double)
   let rs = gain/loss 
-  let rsi  = (100 - 100 /(1+rs))
+  let rsi  = (100 - (100 /(1+rs)))
   return (round rsi,"")
