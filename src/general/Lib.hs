686b92cd73a78ba0f76ce4d86289f8ab6bcfc0f1
yingyith <yithorigin@aprilthirty60.com>
2022-03-13 07:11:04 +0000

    alter now

--- src/general/Lib.hs
+++ src/general/Lib.hs
@@ -14,17 +14,9 @@ import Analysistructure
 
 getrsi :: [Hlnode] -> IO (Int,String)
 getrsi hl = do 
-  liftIO $ print "enter getesi"
-  liftIO $ print hl
-  let klen = DL.length hl
-  liftIO $ print klen
+  let klen = 8
   let updiff   =  [(cprice $ (!!i) al) - (cprice $ (!!(i-1)) al  ) | i <- [1,klen-1] ,let idiff = (cprice $ (!!i) al)-(cprice $ (!!(i-1)) al) in idiff > 0] where al = hl
   let downdiff =  [(cprice $ (!!i) al) - (cprice $ (!!(i-1)) al  ) | i <- [1,klen-1] ,let idiff = (cprice $ (!!i) al)-(cprice $ (!!(i-1)) al) in idiff < 0] where al = hl
-  liftIO $ print "enter getesi2"
-  liftIO $ print "+++++++++++++"
-  liftIO $ print updiff
-  liftIO $ print hl
-  liftIO $ print "+++++++++++++"
   let gain = sum updiff
   let loss = abs $ sum downdiff
   let rs = gain/loss 
