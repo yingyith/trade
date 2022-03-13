37ef5fb86d9acca84432d52edd444d8f26ed7aca
yingyith <yithorigin@aprilthirty60.com>
2022-03-13 06:25:46 +0000

    alter now

--- src/general/Lib.hs
+++ src/general/Lib.hs
@@ -8,21 +8,23 @@ import Data.Aeson
 import Data.Text
 import Data.List as DL
 import Httpstructure
+import Control.Monad
+import Control.Monad.IO.Class
 import Analysistructure
 
 getrsi :: [Hlnode] -> IO (Int,String)
 getrsi hl = do 
-  print "enter getesi"
-  print hl
+  liftIO $ print "enter getesi"
+  liftIO $ print hl
   let klen = DL.length hl
-  print klen
+  liftIO $ print klen
   let updiff   =  [(cprice $ (!!i) al) - (cprice $ (!!(i-1)) al  ) | i <- [1,klen-1] ,let idiff = (cprice $ (!!i) al)-(cprice $ (!!(i-1)) al) in idiff > 0] where al = hl
   let downdiff =  [(cprice $ (!!i) al) - (cprice $ (!!(i-1)) al  ) | i <- [1,klen-1] ,let idiff = (cprice $ (!!i) al)-(cprice $ (!!(i-1)) al) in idiff < 0] where al = hl
-  print "enter getesi2"
-  print "+++++++++++++"
-  print updiff
-  print hl
-  print "+++++++++++++"
+  liftIO $ print "enter getesi2"
+  liftIO $ print "+++++++++++++"
+  liftIO $ print updiff
+  liftIO $ print hl
+  liftIO $ print "+++++++++++++"
   let gain = sum updiff
   let loss = abs $ sum downdiff
   let rs = gain/loss 
