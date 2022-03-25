8b1aef93a70b0b288aeeb74c1bebda17afafd542
yingyith <yithorigin@aprilthirty60.com>
2022-03-25 02:46:59 +0000

    alter now

--- app/Main.hs
+++ app/Main.hs
@@ -149,7 +149,7 @@ ws connection = do
     let ordervari = Ordervar True 0 0 0
     let orderVar = newTVarIO ordervari-- newTVarIO Int
 
-    --void.forkIO $ forever (sendbye conn connection)
+    void.forkIO $  (sendbye conn connection)
 
     void.forkIO . withAsync (publishThread conn connection orderVar) $ \_pubT -> do
                     withAsync (handlerThread conn ctrl orderVar) $ \_handlerT -> do
@@ -160,6 +160,6 @@ ws connection = do
                        void $ addChannels ctrl [] [("analysis:*", analysisHandler)]
 
 
-    void . forkIO $ forever (sendbye conn connection)
+    --void . forkIO $ forever (sendbye conn connection)
     --liftIO $ print ("it is ----!!!!")
 
