f5950aa47cad2d318bbd2995f79a35e380dceae5
yingyith <yithorigin@aprilthirty60.com>
2022-04-29 04:14:36 +0000

    try test depth

--- app/Main.hs
+++ app/Main.hs
@@ -111,7 +111,8 @@ main =
     runRedis conn (liskeytoredis aas nowtime)
     --let aimss = "/stream?streams=adausdt@kline_1m&listenkey=" ++ aas -----------------------------------------------
     --liftIO $ print (aimss)
-    let aimss = "/stream?streams=adausdt@kline_1m/adausdt@depth"  ++ aas -----------------------------------------------
+    let aimss = "/stream?streams=adausdt@kline_1m/adausdt@depth@500ms"  ++ aas -----------------------------------------------
+    --let aimss = "/stream?streams=adausdt@kline_1m/adausdt@depth@500ms"  ++ aas -----------------------------------------------
    -- let aimss = "/stream?streams=adausdt@kline_1m/"  -----------------------------------------------
     --let aimss = "/ws/" ++aas -----------------------------------------------
     --let aimss = "/ws/streams=btcusdt@markPrice/"++ "&listenkey="  ++ aas -----------------------------------------------
