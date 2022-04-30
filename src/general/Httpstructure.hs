38e54fd06f469d164933d1ae52e8f66282d7fe35
yingyith <yithorigin@aprilthirty60.com>
2022-04-30 10:42:39 +0000

    test depth

--- src/general/Httpstructure.hs
+++ src/general/Httpstructure.hs
@@ -206,7 +206,7 @@ queryforder = do
       liftIO $ logact logByteStringStdout $ BC.pack  $ show ("queryorder ----",borders,"+++++",sorders)
       return ()
 
-querydepth :: IO ( [Value])
+querydepth :: IO ( Value)
 querydepth = do
    let symbol = "ADAUSDT"
    let symboll = "ADAUSDT"
@@ -230,7 +230,8 @@ querydepth = do
       let areq = req GET url NoReqBody jsonResponse  httpparams
       response  <- areq
       let result = responseBody response :: Value
-      let sorders = (result^..values)
+      let sorders = (result )
+      --let sorders = (result ^..values)
       --let sorders = (result ^..values).(^? nth 0) 
       return (sorders)
 
