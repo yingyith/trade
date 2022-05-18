b54e06f3d9822617cd76080d3a16165ecd53c2fd
yingyith <yithorigin@aprilthirty60.com>
2022-05-18 08:08:02 +0000

    test double commit

--- src/general/Globalvar.hs
+++ src/general/Globalvar.hs
@@ -41,7 +41,7 @@ diffspreadsheet :: [Double]
 diffspreadsheet = [-0.1 ,0.1  ,0.2  ,0.4  ,0.8  ,1.2  ,1.6  ,2] 
 
 depthrisksheet :: [Int] 
-depthrisksheet = [100  ,120  ,140 , 160 , 180, 200,220  ]   -- 
+depthrisksheet = [100  ,120  ,160 , 200 , 300, 400 , 520  ]   -- 
 
 biddepth = "Biddepth"
 askdepth = "Askdepth"
