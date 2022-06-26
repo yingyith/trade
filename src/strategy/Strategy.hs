a95b9ddbf518d1667cab23561e4ad651892990d8
yingyith <yithorigin@aprilthirty60.com>
2022-06-26 21:39:04 +0000

    update for two side test

--- src/strategy/Strategy.hs
+++ src/strategy/Strategy.hs
@@ -206,8 +206,8 @@ minrule ahll pr interval  = do
    case (fastprevuppredi,fastprevdopredi,bigpredi) of 
         (True  ,False ,_     ) ->  return ((( (!!2) $ fromJust $  minrisksheet!?interval),gridspan),("uf",rsiindex)) -- down fast
         (False ,True  ,_     ) ->  return ((( (!!1) $ fromJust $  minrisksheet!?interval),gridspan),("df",rsiindex)) -- down fast
-        (False ,False ,True  ) ->  return ((0,gridspan),("do",rsiindex)) -- down fast
-        (False ,False ,False ) ->  return ((0,gridspan),("up",rsiindex)) -- down fast
+        (False ,False ,True  ) ->  return ((0,gridspan),("up",rsiindex)) -- down fast
+        (False ,False ,False ) ->  return ((0,gridspan),("do",rsiindex)) -- down fast
         (_     ,_     ,_     ) ->  return ((0,gridspan),("no",0)) -- down fast
    
 
