2316d3d66305665828df5e80cf6c3751773f4d16
yingyith <yithorigin@aprilthirty60.com>
2022-07-04 11:29:34 +0000

    update for two side

--- src/general/Analysistructure.hs
+++ src/general/Analysistructure.hs
@@ -79,11 +79,11 @@ getBidAskNum apr dpdata = [(sum $ DHM.elems $  DHM.filterWithKey  (getbiddiffqua
                            sum $ DHM.elems $  DHM.filterWithKey  (getaskdiffquanpred  (snd apr) 0.0002 ) $ askset  dpdata 
                            ),
                            (
-                           sum $ DHM.elems $  DHM.filterWithKey  (getaskdiffquanpred  ((+ 0.0002) $ snd apr) 0.0002 ) $ bidset  dpdata, 
-                           sum $ DHM.elems $  DHM.filterWithKey  (getaskdiffquanpred  ((snd apr)-0.0002) 0.0002 ) $ askset  dpdata 
+                           sum $ DHM.elems $  DHM.filterWithKey  (getbiddiffquanpred  ((snd apr)-0.0002) 0.0002 ) $ bidset  dpdata, 
+                           sum $ DHM.elems $  DHM.filterWithKey  (getaskdiffquanpred  ((snd apr)+0.0002) 0.0002 ) $ askset  dpdata 
                            ),
-                           (sum $ DHM.elems $  DHM.filterWithKey  (getaskdiffquanpred ((+ 0.0004) $  snd apr) 0.0002 ) $ bidset  dpdata,
-                           sum $ DHM.elems $  DHM.filterWithKey  (getaskdiffquanpred  ((snd apr)-0.0004 ) 0.0002 ) $ askset  dpdata 
+                           (sum $ DHM.elems $  DHM.filterWithKey  (getbiddiffquanpred ((snd apr)-0.0002) 0.0002 ) $ bidset  dpdata,
+                           sum $ DHM.elems $  DHM.filterWithKey  (getaskdiffquanpred  ((snd apr)+0.0004) 0.0002 ) $ askset  dpdata 
                            ),
                            ( 0,
                              0 
