75d0fd6b19df7895a5f7fdeafcaf1e9c81b92f77
yingyith <yithorigin@aprilthirty60.com>
2022-07-16 11:58:19 +0000

    test needle strategy

--- src/strategy/Strategy.hs
+++ src/strategy/Strategy.hs
@@ -204,10 +204,14 @@ genehighlowsheet index hl key = do
     let lpointpredication = (curitemlp - nextitemlp) <= 0
     let predication = (hpointpredication,lpointpredication)
     let res = case predication of 
-                  (True,True)   ->  (AS.Hlnode curitemt 0         curitemlp     0 "low"    key curitemcp)
-                  (False,False) ->  (AS.Hlnode curitemt curitemhp 0             0 "high"   key curitemcp)
+                 -- (True,True)   ->  (AS.Hlnode curitemt 0         curitemlp     0 "low"    key curitemcp)
+                 -- (False,False) ->  (AS.Hlnode curitemt curitemhp 0             0 "high"   key curitemcp)
+                 -- (False,True)  ->  (AS.Hlnode curitemt curitemhp curitemlp     0 "wbig"   key curitemcp)
+                 -- (True,False)  ->  (AS.Hlnode curitemt 0         0             0 "wsmall" key curitemcp)
+                  (True,True)   ->  (AS.Hlnode curitemt curitemhp curitemlp     0 "low"    key curitemcp)
+                  (False,False) ->  (AS.Hlnode curitemt curitemhp curitemlp     0 "high"   key curitemcp)
                   (False,True)  ->  (AS.Hlnode curitemt curitemhp curitemlp     0 "wbig"   key curitemcp)
-                  (True,False)  ->  (AS.Hlnode curitemt 0         0             0 "wsmall" key curitemcp)
+                  (True,False)  ->  (AS.Hlnode curitemt curitemhp curitemlp     0 "wsmall" key curitemcp)
     return res
 
 minrule :: [AS.Hlnode]-> Double-> String  -> IO ((Int,(Double,Double)),(String,Int))
