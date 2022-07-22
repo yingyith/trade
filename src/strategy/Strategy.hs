d8443c230690078819aa9ae3c341e70127945187
yingyith <yithorigin@aprilthirty60.com>
2022-07-22 09:27:26 +0000

    test for 1m str

--- src/strategy/Strategy.hs
+++ src/strategy/Strategy.hs
@@ -220,7 +220,7 @@ minrule ahll pr interval  = do
    let alength = case interval of 
                     "3m" -> 7
                     "5m" -> 7
-                    "4h" -> 16
+                    "4h" -> 14
                     _    -> 11
    let ahl = DT.take alength ahll
    let reslist   =  [(xlist!!x,x)|x<-[0..(length xlist-2)]] where xlist = ahl
