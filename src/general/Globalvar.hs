ea5a07ecc911e4ac347d3872de11b2e46b11deea
yingyith <yithorigin@aprilthirty60.com>
2022-08-15 23:47:50 +0000

    test for dynamic grid

--- src/general/Globalvar.hs
+++ src/general/Globalvar.hs
@@ -43,10 +43,10 @@ stopprofitlist          :: [Double]
 stopprofitlist          = [0.0006,0.001,0.0014,0.0018,0.0022,0.0026] -- 15m,1h,4h,12h
 
 diffspreadsheet         :: [Double]
-diffspreadsheet         = [0.01   ,0.26   ,0.4111  ,0.52  ,0.603  ,0.72  ,0.81  ,0.95  ,1] 
+diffspreadsheet         = [0.01   ,0.25   ,0.34  ,0.5  ,0.603  ,0.72  ,0.81  ,0.95  ,1] 
 
 depthrisksheet          :: [Int] 
-depthrisksheet          = [-3000  ,150   ,310   ,560   ,1110  ,2060  ,2550  ,2800 ]   --deothrisksheet x 2  = minrulethreshold + shortminrulethreshold 
+depthrisksheet          = [-3000  ,150   ,410   ,630   ,1110  ,2060  ,2550  ,2800 ]   --deothrisksheet x 2  = minrulethreshold + shortminrulethreshold 
 
 minrulethreshold        :: [Int] --base  to serious degree
 minrulethreshold        = [2000  ,1600  ,1200  ,1100  ]   --   4h, 1h,15m -->    d,d,d  ;u,u,u  -> 2000
@@ -61,7 +61,7 @@ adjustratiosheet :: [Double]
 adjustratiosheet        = [0.001   , 0.2        , 0.3        ,  0.57      , 0.62          , 0.76        , 0.9                , 1     ]
 
 adjustboostgrid :: [(Int,Int)]
-adjustboostgrid         =  [(0,300) ,(400,400)   ,(1200,1200)   , (1600,2500), (2510,3000)  ,(3000,4000) , (4000,5000)               ]
+adjustboostgrid         =  [(0,300) ,(640,640)   ,(1200,1200)   , (1600,2500), (2510,3000)  ,(3000,4000) , (4000,5000)               ]
 
 biddepth = "Biddepth"
 askdepth = "Askdepth"
