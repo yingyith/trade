ea61487eb14fe4f8cc600faf9dd39758990b3464
yingyith <yithorigin@aprilthirty60.com>
2022-08-18 04:01:57 +0000

    test for dynamic grid

--- src/strategy/Strategy.hs
+++ src/strategy/Strategy.hs
@@ -189,7 +189,7 @@ suddenwavestra  abcc  =  case (vo_wave_pred_15m,vo_wave_pred_1h,vo_wave_pred_4h)
                                                     (False,True ,False) -> (3000,"no")
                                                     (False,True ,True ) -> (3000,"no")
                                                     (True ,True ,False) -> (4000,"no")
-                                                    (True ,True ,True ) -> (9000,"no")
+                                                    (True ,True ,True ) -> (4000,"no")
                                                     (False,False,True ) -> (3000,"no")
                                                     (False,False,False) -> (0  ,"yes")
                          where 
@@ -538,7 +538,7 @@ secondrule diffpr ablist = do      -- bid is buyer , ask is seller
                                                                       AS.ND  -> (1,"pr")
                                                                       AS.DO  -> (1,"pr1")
                                                                       AS.UP  -> (1,"cc2")
-                                                          x|x>0.5 -> (1,"no") 
+                                                          x|x>0.6 -> (1,"no") 
                                                           _       -> case (fst ccctrend) of 
                                                                       AS.ND ->  (1,"no")
                                                                       AS.DO  -> (1,"cc1")
@@ -548,7 +548,7 @@ secondrule diffpr ablist = do      -- bid is buyer , ask is seller
                                                                       AS.ND  -> (1,"pr")
                                                                       AS.UP  -> (1,"pr1")
                                                                       AS.DO  -> (1,"cc2")
-                                                          x|x>0.5 -> (1,"no") 
+                                                          x|x>0.6 -> (1,"no") 
                                                           _       -> case (fst ccctrend) of
                                                                       AS.ND ->  (1,"no")
                                                                       AS.UP  -> (1,"cc1")
