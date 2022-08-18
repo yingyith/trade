219060ff80085ab3a96dad1b82f3c29b0527dec0
yingyith <yithorigin@aprilthirty60.com>
2022-08-18 22:57:20 +0000

    test for dynamic grid

--- src/general/Analysistructure.hs
+++ src/general/Analysistructure.hs
@@ -126,8 +126,8 @@ getBidAskNum apr dpdata = [
                            (sum $ DHM.elems $  DHM.filterWithKey  (getbiddiffquanpred (snd apr) 0.008 ) $ bidset  dpdata ,
                            sum $ DHM.elems $  DHM.filterWithKey  (getaskdiffquanpred  (snd apr) 0.008 ) $ askset  dpdata 
                            ),
-                           (sum $ DHM.elems $  DHM.filterWithKey  (getbiddiffquanpred (snd apr) 0.02 ) $ bidset  dpdata ,
-                           sum $ DHM.elems $  DHM.filterWithKey  (getaskdiffquanpred  (snd apr) 0.02 ) $ askset  dpdata 
+                           (sum $ DHM.elems $  DHM.filterWithKey  (getbiddiffquanpred (snd apr) 0.012 ) $ bidset  dpdata ,
+                           sum $ DHM.elems $  DHM.filterWithKey  (getaskdiffquanpred  (snd apr) 0.012 ) $ askset  dpdata 
                            ),
                            (0 ,
                             0 
