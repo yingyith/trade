da1f1704e6c5d02b82fc5db13399eff4156e06c3
yingyith <yithorigin@aprilthirty60.com>
2022-09-11 13:18:04 +0000

    test for ab and ba

--- src/general/Analysistructure.hs
+++ src/general/Analysistructure.hs
@@ -126,16 +126,15 @@ getBidAskNum apr dpdata = [
                            ( 0,
                              0
                            ),
-                           (
-                           sum $ DHM.elems $  DHM.filterWithKey  (getbiddiffquanpred (snd apr) 0.0002 ) $ bidset  dpdata ,
-                           sum $ DHM.elems $  DHM.filterWithKey  (getaskdiffquanpred  (snd apr) 0.0002 ) $ askset  dpdata 
+                           ( 0,
+                             0
                            ),
                            (
                            sum $ DHM.elems $  DHM.filterWithKey  (getbiddiffquanpred (snd apr) 0.0001 ) $ bidset  dpdata ,
                            sum $ DHM.elems $  DHM.filterWithKey  (getaskdiffquanpred  (snd apr) 0.0001 ) $ askset  dpdata 
                            ),
-                           (sum $ DHM.elems $  DHM.filterWithKey  (getbiddiffquanpred (snd apr) 0.0004 ) $ bidset  dpdata ,
-                           sum $ DHM.elems $  DHM.filterWithKey  (getaskdiffquanpred  (snd apr) 0.0004 ) $ askset  dpdata 
+                           (sum $ DHM.elems $  DHM.filterWithKey  (getbiddiffquanpred (snd apr) 0.0002 ) $ bidset  dpdata ,
+                           sum $ DHM.elems $  DHM.filterWithKey  (getaskdiffquanpred  (snd apr) 0.0002 ) $ askset  dpdata 
                            ),
                            (sum $ DHM.elems $  DHM.filterWithKey  (getbiddiffquanpred (snd apr) 0.004 ) $ bidset  dpdata ,
                            sum $ DHM.elems $  DHM.filterWithKey  (getaskdiffquanpred  (snd apr) 0.004 ) $ askset  dpdata 
