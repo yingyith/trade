ed7a8f0559fd4c9154d8830f1de0cfbcd69638fd
yingyith <yithorigin@aprilthirty60.com>
2022-09-15 13:28:57 +0000

    branch trendwave_1 and trend_wave_2

--- src/strategy/Strategy.hs
+++ src/strategy/Strategy.hs
@@ -429,7 +429,7 @@ trendwave_1 :: AS.Trend -> Double -> Double  -> Double -> Double -> Double -> AS
 trendwave_1  trd  fab  fba  ab  ba  aaa  basetrd   =   case trd of    -- return +0.0001 ratio as  base
                                                   AS.DO -> case (fba < fab-0.1) of 
                                                               False   ->  (False,"1","no",0,0)
-                                                              True    ->  case (ba < ab-0.1) && (aaa <(-0.1)) of 
+                                                              True    ->  case (ba < ab-0.2) && (aaa <(-0.1)) of 
                                                                             False -> (False,"1","no",0,0)
                                                                             True  -> case ba of
                                                                                                x|x<(-0.2) -> case (basetrd) of 
@@ -442,7 +442,7 @@ trendwave_1  trd  fab  fba  ab  ba  aaa  basetrd   =   case trd of    -- return 
                                                                                                                AS.UP  -> (False,"1","no",0,0)
                                                   AS.UP -> case (fab < fba-0.1) of
                                                               False   ->  (False,"1","no",0,0)
-                                                              True    ->  case (ab < ba-0.1) && (aaa > 0.1) of 
+                                                              True    ->  case (ab < ba-0.2) && (aaa > 0.1) of 
                                                                             False -> (False,"1","no",0,0)
                                                                             True  -> case ab of
                                                                                                x|x<(-0.2) -> case (basetrd) of 
