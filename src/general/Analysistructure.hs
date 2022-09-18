d0044ab178c05dbf8dca3da1d55a4949a379ea71
yingyith <yithorigin@aprilthirty60.com>
2022-09-18 06:32:48 +0000

    update for normal sndrule record

--- src/general/Analysistructure.hs
+++ src/general/Analysistructure.hs
@@ -131,11 +131,11 @@ getBidAskNum apr dpdata = [
                             0
                            ),
                            (
-                           sum $ DHM.elems $  DHM.filterWithKey  (getbiddiffquanpred  (snd apr) 0.0002 ) $ bidset  dpdata ,
-                           sum $ DHM.elems $  DHM.filterWithKey  (getaskdiffquanpred  (snd apr) 0.0002 ) $ askset  dpdata 
+                           sum $ DHM.elems $  DHM.filterWithKey  (getbiddiffquanpred  (snd apr) 0.0004 ) $ bidset  dpdata ,
+                           sum $ DHM.elems $  DHM.filterWithKey  (getaskdiffquanpred  (snd apr) 0.0004 ) $ askset  dpdata 
                            ),
-                           (sum $ DHM.elems $  DHM.filterWithKey  (getbiddiffquanpred (snd apr) 0.0006 ) $ bidset  dpdata ,
-                           sum $ DHM.elems $  DHM.filterWithKey  (getaskdiffquanpred  (snd apr) 0.0006 ) $ askset  dpdata 
+                           (sum $ DHM.elems $  DHM.filterWithKey  (getbiddiffquanpred (snd apr) 0.0007 ) $ bidset  dpdata ,
+                           sum $ DHM.elems $  DHM.filterWithKey  (getaskdiffquanpred  (snd apr) 0.0007 ) $ askset  dpdata 
                            ),
                            (sum $ DHM.elems $  DHM.filterWithKey  (getbiddiffquanpred (snd apr) 0.004 ) $ bidset  dpdata ,
                            sum $ DHM.elems $  DHM.filterWithKey  (getaskdiffquanpred  (snd apr) 0.004 ) $ askset  dpdata 
